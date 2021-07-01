4ad0d66a9306793989298c07ff443a6deb063f77
victoria okumu <victoria.okumu@student.moringaschool.com>
2021-07-01 11:06:18 +0300

    Added validation content

---
+++ js/validation.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    $('#loginForm').validate({ // initialize the plugin
+        rules: {
+            NAME: {
+                required: true,
+            },
+            EMAIL: {
+                required: true,
+                email: true
+            },
+            PASSWORD: {
+                required: true,
+            }
+        },
+        messages : {
+            NAME: {
+               required: "Username is required",
+            },
+            EMAIL: {
+                required: "Email is required",
+                email: "The email should be in the format: abc@domain.tld"
+            },
+            PASSWORD: {
+                required: "Password is required",
+            }
+            }
+    });
+
+});
